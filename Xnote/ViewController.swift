87cfb2ac3b9b920f5d8770e0a7a4ff30f767a802
Acjota Merma Cesar Augusto <cesar.acjota@tecsup.edu.pe>
2021-12-01 22:30:02 -0800

    Initial Commit

---
+++ Xnote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xnote
+//
+//  Created by Cesar Augusto Acjota Merma on 12/1/21.
+//  Copyright © 2021 xnote. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
